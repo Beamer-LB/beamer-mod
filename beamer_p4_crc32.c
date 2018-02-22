81ea2c424a49d821ecdaacd707adc9d2451bc619
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2018-02-22 02:39:40 +0200

    barefoot license

--- beamer_p4_crc32.c
+++ beamer_p4_crc32.c
@@ -1,4 +1,18 @@
-//TODO: license (barefoot)
+/* Copyright 2013-present Barefoot Networks, Inc.,
+ * 2018 University Politehnica of Bucharest
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include "beamer_p4_crc32.h"
 
